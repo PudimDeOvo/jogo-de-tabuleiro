f3294eb12c8780a9ff52b1691e8e84442a9ee24a
Camila P. Dias <diascamila2005@gmail.com>
2025-05-15 22:43:36 -0300

    Update NoNextMoveTile.java

--- src/Tile/NoNextMoveTile.java
+++ src/Tile/NoNextMoveTile.java
@@ -2,13 +2,13 @@ package Tile;
 import Player.Player;
 import java.util.ArrayList;
 
-public class NoNextMoveTile extends Tile{
+public class NoNextMoveTile extends Tile {
     public NoNextMoveTile(){
         super();
     }
 
     public void effect(ArrayList<Player> players, int index){
-        //System.out.println("No next move tile! You lose your next turn!");
-        //players.get(index).setNextMove(false);
+        System.out.println("No next move tile! You lose your next turn!\n");
+        players.get(index).setNextMove(false);
     }
 }
