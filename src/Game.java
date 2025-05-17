0ff61e1dcba2893320131cd9a6baf505e6a9471f
Camila P. Dias <diascamila2005@gmail.com>
2025-05-17 00:20:10 -0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+
 public class Game {
     private int turn = 1;
     private final Gameboard gameboard = new Gameboard();
@@ -22,13 +23,13 @@ public class Game {
             Player newPlayer = null;
             switch(playerType){
                 case 1:
-                    newPlayer = new RegularPlayer(colors.get(i), 0, true);
+                    newPlayer = new RegularPlayer(colors.get(i), 0, true, 0);
                     break;
                 case 2:
-                    newPlayer = new LuckyPlayer(colors.get(i), 0, true);
+                    newPlayer = new LuckyPlayer(colors.get(i), 0, true, 0);
                     break;
                 case 3:
-                    newPlayer = new UnluckyPlayer(colors.get(i), 0, true);
+                    newPlayer = new UnluckyPlayer(colors.get(i), 0, true, 0);
                     break;
                 default:
                     System.out.println("Invalid player type. Please choose again. \n");
@@ -60,30 +61,52 @@ public class Game {
     public void listPlayers(ArrayList<Player> players){
         System.out.println("List of players: \n");
         for (int i = 0; i < players.size(); i++){
-            System.out.println("P" + (i+1) + ": " + players.get(i).getColor() + " - " + players.get(i).getPlayerType() + "\n");
+            System.out.println("P" + (i+1) + ": " + players.get(i).getColor() + " - " + players.get(i).getPlayerType() + " Position: " + players.get(i).getPosition() + "\n");
         }
     }
 
     // marcello disse: vou refinar essa buceta aqui depois. estou enlouquecendo. botar essa bct de funcao dentro de main
-    // mas falta oq? ta bom ne nao
+    // mds na vdd to enlouquecendo tambem!!!
     public void playerTurn(ArrayList<Player> players, int playerIndex, Gameboard gameboard){
         int playerRoll = players.get(playerIndex).roll();
 
-        players.get(playerIndex).setPosition(
-                players.get(playerIndex).getPosition() + playerRoll
-        );
+        if(players.get(playerIndex).getPosition() + playerRoll <= 40) {
+            players.get(playerIndex).setPosition(
+                    players.get(playerIndex).getPosition() + playerRoll
+            );
+            gameboard.getBoard().get(
+                    players.get(playerIndex).getPosition()
+            ).effect(players, playerIndex);
 
-        gameboard.getBoard().get(
-                players.get(playerIndex).getPosition()
-        ).effect(players, playerIndex);
+            players.get(playerIndex).setCountPlays(
+                    players.get(playerIndex).getCountPlays() + 1
+            );
+        }
+        else{
+            players.get(playerIndex).setPosition(40);
+
+            players.get(playerIndex).setCountPlays(
+                    players.get(playerIndex).getCountPlays() + 1
+            );
+        }
     }
 
     public void startGame(ArrayList<Player> players, int playerIndex){
         while (true) {
             System.out.println("Turn " + turn + "\n");
+
+            if (players.get(playerIndex).getPosition() >= 40) {
+                System.out.println("Player " + players.get(playerIndex).getColor() + " wins! \n");
+                break;
+            }
+
             if(players.get(playerIndex).getNextMove()){
                 System.out.println("Current player: " + players.get(playerIndex).getColor() + "\n");
                 playerTurn(players, playerIndex, gameboard);
+                if (players.get(playerIndex).getPosition() >= 40) {
+                    System.out.println("Player " + players.get(playerIndex).getColor() + " wins! \n");
+                    break;
+                }
                 listPlayers(players);
             }
             else{
@@ -91,22 +114,18 @@ public class Game {
                 players.get(playerIndex).setNextMove(true);
             }
 
-            if (players.get(playerIndex).getPosition() >= 40) {
-                System.out.println("Player " + players.get(playerIndex).getColor() + " wins! \n");
-                break;
+            playerIndex++;
+            if(playerIndex >= players.size()){
+                playerIndex = 0;
+                turn++;
             }
-
-            playerIndex += 1;
-            turn++;
         }
-        // ganhou ai mostra a lista dos jogador e o vencedor edepois reinicia o jogo
         System.out.println("Game over! \n");
         System.out.println("List of players and the count of each one's plays: \n");
-        listPlayers(players);
-        // nao pensei em como contar as jogadas
-        // se pa criando uma lista de contadores mesmo? dai o index representa o jogador
-        // exceto que quando dois jogadores trocarem de lugar tem que trocar o index dos contadores tambem ???
-        // pensando...
+        for(int i = 0; i < players.size(); i++){
+            System.out.println("Player: " + players.get(i).getColor() + " in position: " + players.get(i).getPosition() + " made " + players.get(i).getCountPlays() + " plays.\n");
+        }
+        // o contador de jogadas contei como outro atributo pq se fosse uma variavel ia ficar paia de mexer nos index
     }
 
 }
