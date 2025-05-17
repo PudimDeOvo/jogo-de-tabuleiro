2570a03ea5563cabd4b7a7e05c812a1a5ec29af9
Camila P. Dias <diascamila2005@gmail.com>
2025-05-17 00:23:11 -0300

    Update LuckyTile.java

--- src/Tile/LuckyTile.java
+++ src/Tile/LuckyTile.java
@@ -1,23 +1,25 @@
 package Tile;
 import Player.Player;
-import Player.UnluckyPlayer;
 import java.util.ArrayList;
 
-public class LuckyTile extends Tile{
-    public LuckyTile(){
+public class MagicTile extends Tile{
+    public MagicTile() {
         super();
     }
 
-    public void effect(ArrayList<Player> players, int index){
-        if (!(players.get(index) instanceof UnluckyPlayer)){
+    public void effect(ArrayList<Player> players, int index) {
+        System.out.println("Magic tile! Now, you're switching positions with the player most behind: ");
+        int indexSwapPlayer = 1;
+        for(int i = 0; i < players.size(); i++){
+            if (players.get(i).getPosition() < players.get(indexSwapPlayer).getPosition()){
+                indexSwapPlayer = i;
+            }
+        }
+        System.out.println("Woooohh! Swapping positions between player "+ players.get(index).getColor() + " and player " + players.get(indexSwapPlayer).getColor() + "!");
 
-            System.out.println("Lucky tile! Walking three more tiles!");
-            players.get(index).setPosition(
-                    players.get(index).getPosition() + 3
-            );
-        }
-        else {
-            System.out.println("Lucky tile! Too bad you're unlucky!");
-        }
+        int temp1 = players.get(index).getPosition();
+        int temp2 = players.get(indexSwapPlayer).getPosition();
+        players.get(index).setPosition(temp2);
+        players.get(indexSwapPlayer).setPosition(temp1);
     }
 }
